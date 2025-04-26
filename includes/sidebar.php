0655357f1c824d18dad7eb297934ec4452d5e3b2
John Rae G. Manuel <159091163+johnraemanuel09@users.noreply.github.com>
2025-04-26 15:17:48 +0800

    updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -310,6 +310,33 @@
                   <span class="nav-link-text ms-1">Add Alumni</span>
                 </a>
               </li>
+
+              <li class="nav-item ">
+                <a class="nav-link text-white " href="../../pages/custodian/add.custodian.php">
+                  <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class="material-icons opacity-10">person_add</i>
+                  </div>
+                  <span class="nav-link-text ms-1">Add Custodian</span>
+                </a>
+              </li>
+
+              <li class="nav-item ">
+                <a class="nav-link text-white " href="../../pages/teacher/add.teacher.php">
+                  <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class="material-icons opacity-10">person_add</i>
+                  </div>
+                  <span class="nav-link-text ms-1">Add Teacher</span>
+                </a>
+              </li>
+
+              <li class="nav-item ">
+                <a class="nav-link text-white " href="../../pages/teacher/add.student.php">
+                  <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class="material-icons opacity-10">person_add</i>
+                  </div>
+                  <span class="nav-link-text ms-1">Add Student</span>
+                </a>
+              </li>
             </ul>
           </div>
         </li>
